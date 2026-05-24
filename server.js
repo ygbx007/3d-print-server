00fdb26e6da174334bd864e2d8d9595c29131e74
ygbx007 <chwygbx@gmail.com>
2026-05-25 02:49:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ async function writeRecordToBase(token, recordData) {
                 "模型颜色/参数": recordData.modelParams || "",
                 "积分币数量": parseInt(recordData.coins) || 0,
                 "积分币是否已给": recordData.coinsPaid || "否"
+                "预约时间": Date.now()
             }
         });
 
